07dbc33d6f23adb81bcd0d55e35be5d390207496
vyvahuy123 <vyvahuy123@gmail.com>
2026-04-06 14:23:29 +0700

    feat: add repositories for Payment, ProductImage, Notification, WishList

--- src/Infrastructure/Persistence/UnitOfWork.cs
+++ src/Infrastructure/Persistence/UnitOfWork.cs
@@ -1,4 +1,4 @@
-using Domain.Interfaces;
+﻿using Domain.Interfaces;
 using Infrastructure.Persistence.Repositories;
 
 namespace Infrastructure.Persistence;
@@ -6,9 +6,14 @@ namespace Infrastructure.Persistence;
 public class UnitOfWork : IUnitOfWork
 {
     private readonly AppDbContext _ctx;
+
     private IUserRepository? _users;
     private IProductRepository? _products;
     private IRefreshTokenRepository? _refreshTokens;
+    private IPaymentRepository? _payments;
+    private IProductImageRepository? _productImages;
+    private INotificationRepository? _notifications;
+    private IWishListRepository? _wishLists;
 
     public UnitOfWork(AppDbContext ctx) => _ctx = ctx;
 
@@ -20,6 +25,18 @@ public class UnitOfWork : IUnitOfWork
 
     public IRefreshTokenRepository RefreshTokens
         => _refreshTokens ??= new RefreshTokenRepository(_ctx);
+
+    public IPaymentRepository Payments
+        => _payments ??= new PaymentRepository(_ctx);
+
+    public IProductImageRepository ProductImages
+        => _productImages ??= new ProductImageRepository(_ctx);
+
+    public INotificationRepository Notifications
+        => _notifications ??= new NotificationRepository(_ctx);
+
+    public IWishListRepository WishLists
+        => _wishLists ??= new WishListRepository(_ctx);
 
     public async Task<int> SaveChangesAsync(CancellationToken ct = default)
         => await _ctx.SaveChangesAsync(ct);
